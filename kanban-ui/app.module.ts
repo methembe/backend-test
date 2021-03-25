564e409d0ffc34d7b76fff7a632b570f4b83a8a6
Tashen247 <tashenm@gmail.com>
2021-03-25 18:28:52 +0200

    Add files via upload

---
+++ kanban-ui/app.module.ts
@@ -0,0 +1,58 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatInputModule, MatSelectModule } from "@angular/material";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './home/home.component';
+import { KanbanComponent } from './kanban/kanban.component';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { KanbanDialogComponent } from './kanban-dialog/kanban-dialog.component'; 
+import {  ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    KanbanComponent,
+    TaskDialogComponent,
+    KanbanDialogComponent,
+     ConfirmDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatListModule,
+    DragDropModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSidenavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  entryComponents: [TaskDialogComponent, KanbanDialogComponent, ConfirmDialogComponent]
+})
+export class AppModule { }

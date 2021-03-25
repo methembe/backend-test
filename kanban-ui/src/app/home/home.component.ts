7df47d47e75237f5a4579685901be3152f9c5435
Tashen247 <tashenm@gmail.com>
2021-03-25 18:24:57 +0200

    Add files via upload

--- kanban-ui/src/app/home/home.component.ts
+++ kanban-ui/src/app/home/home.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { Kanban } from '../model/kanban/kanban';
 import { KanbanService } from '../service/kanban-service.service';
-import { MatDialog, MatDialogConfig } from '@angular/material';
+import { MatDialog, MatDialogConfig, MatTableDataSource } from '@angular/material';
 import { KanbanDialogComponent } from '../kanban-dialog/kanban-dialog.component';
+//import { DeleteConfirmDialogComponent, ConfirmDialogModel} from '../shared/Delete-Confirm-Dialog/Delete-Confirm-Dialog.Component';
+import { ConfirmDialogModel, ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
 
 @Component({
   selector: 'app-home',
@@ -13,12 +18,17 @@ export class HomeComponent implements OnInit {
 
   kanbanList: Kanban[];
   kanban: Kanban;
+  result: string = '';
+  dataSource = new MatTableDataSource<Kanban>();
+  displayedColumns: string[] = ["id","Title","CreatedDate","details","delete"]
 
   constructor(
     private kanbanService: KanbanService,
     private dialog: MatDialog
   ) { }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
   ngOnInit() {
     this.retrieveAllKanbanBoards();
   }
@@ -37,6 +47,9 @@ export class HomeComponent implements OnInit {
 
       response => {
         this.kanbanList = response;
+        this.dataSource.data = response;
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
       }
     )
   }
@@ -50,5 +63,21 @@ export class HomeComponent implements OnInit {
     if(confirm("Are you sure to delete "+name)) {
       this.delete(name);
     }
+  }
+
+  confirmDialog(id: number): void {
+    const message = `Are you sure you want to do this?`;
+
+    const dialogData = new ConfirmDialogModel("Confirm Action", message,id);
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      maxWidth: "400px",
+      data: dialogData
+    });
+
+    dialogRef.afterClosed().subscribe(dialogResult => {
+      if (dialogResult)
+        this.delete(id);
+    });
   }
 }
